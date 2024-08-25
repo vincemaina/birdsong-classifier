04b3b1765742f2a23dec60bebe0d8ddfde9df870
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-25 18:37:08 +0100

    feat: add desired birds

---
+++ helpers/constants.py
@@ -0,0 +1,12 @@
+birds = [
+    ("Fringilla", "coelebs"),  # Chaffinch
+    ("Parus", "major"),  # Great Tit
+    ("Turdus", "merula"),  # Blackbird
+    ("Erithacus", "rubecula"),  # Robin
+    ("Columba", "palumbus"),  # Wood Pigeon
+    ("Turdus", "philomelos"),  # Song Thrush
+    ("Cyanistes", "caeruleus"),  # Blue Tit
+    ("Cuculus", "canorus"),  # Cuckoo
+    ("Troglodytes", "troglodytes"),  # Wren
+    ("Sylvia", "atricapilla")  # Eurasian Blackcap
+]
