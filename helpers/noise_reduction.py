614fbb6370decbbbf3b272a7009f8f5dad460d55
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-27 16:37:53 +0100

    feat: add helper function for noise reduction

---
+++ helpers/noise_reduction.py
@@ -0,0 +1,16 @@
+import noisereduce as nr
+
+def apply_spectral_gate(y, sr):
+    # Apply spectral gating to reduce noise
+    y = nr.reduce_noise(y, sr)
+    return y
+
+
+if __name__ == "__main__":
+    import librosa
+
+    # Load audio file
+    y, sr = librosa.load('data/bird_recordings/25745')
+
+    # Apply spectral gating to reduce noise
+    y = apply_spectral_gate(y, sr)
