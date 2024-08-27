2e0fc72d139fc6885176239afc479936e610883e
Vince Maina <vincemaina@proton.me>
2024-08-27 19:29:47 +0100

    refactor: specify output type

--- helpers/noise_reduction.py
+++ helpers/noise_reduction.py
@@ -1,7 +1,9 @@
+from numpy import ndarray
 import noisereduce as nr
 
-def apply_spectral_gate(y, sr):
-    # Apply spectral gating to reduce noise
+
+def apply_spectral_gate(y, sr) -> ndarray:
+    """Apply spectral gating to reduce noise"""
     y = nr.reduce_noise(y, sr)
     return y
 
@@ -10,7 +12,7 @@ if __name__ == "__main__":
     import librosa
 
     # Load audio file
-    y, sr = librosa.load('data/bird_recordings/25745')
+    y, sr = librosa.load("data/bird_recordings/25745")
 
     # Apply spectral gating to reduce noise
     y = apply_spectral_gate(y, sr)
