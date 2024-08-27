53bce68980a08e6bd7f3ef3d8bdfab516b76ee96
Vince Chapman <95453696+vincechapman@users.noreply.github.com>
2024-08-27 16:38:06 +0100

    feat: add helper function for normalizing audio

--- helpers/util.py
+++ helpers/util.py
@@ -4,3 +4,6 @@ SAMPLE_RATE = 16000
 
 def load_audio(id):
     return librosa.load(f"data/bird_recordings/{id}", mono=True, sr=SAMPLE_RATE)
+
+def normalize_audio(y):
+    return librosa.util.normalize(y)
