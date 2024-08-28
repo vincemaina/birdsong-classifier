4fd99be84c8aae049a2aeeec64bc71992e7eeb63
Vince Maina <vincemaina@proton.me>
2024-08-28 15:10:41 +0100

    feat: create meta model file

---
+++ models/0_meta_model/__init__.py
@@ -0,0 +1,12 @@
+import xgboost as xgb
+
+
+class MetaModel:
+    """A stacked model trained on various submodels."""
+
+    def __init__(self):
+        print("MetaModel loaded.")
+
+
+if __name__ == "__main__":
+    model = MetaModel()
